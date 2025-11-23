0189fb7331d08c1b1ef0ca0ff9eb31ec2d7749a8
ailinol <lilianolicumba42@gmail.com>
2025-11-23 10:52:53 +0200

    First Commit

---
+++ src/main/java/com/example/greenMach/GreenMachApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenMach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenMachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenMachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/greenMach/GreenMachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenMach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenMachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
